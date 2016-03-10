57be193675efa6141886cd6dff0a8826e069cdcc
0904694 <0904694@users.noreply.github.com>
2016-03-11 06:55:50 +0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -2,55 +2,33 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import scipy as sp
+import copy as cp
+from PIL import Image
 
-#define a function called svd 
-def svd(n):
+
+def reso(n,U,sigma,V):  #compress image 
+    sigma_low=cp.copy(sigma)
+    for j in range(n,len(sigma[:,0])):  
+        sigma_low[j][j]=0; # only keep the first n non-zero element while the rest are set to zeros  
+    A_low=np.asmatrix(U)*np.asmatrix(sigma_low)*np.asmatrix(V) # merge back the decomposition
+    A_low = np.uint8(np.array(A_low))
+    
+    return A_low
+    
+    
+    
+def decomposition(A): # singular value decomposition
+    U, s, V= np.linalg.svd(A, full_matrices=True, compute_uv=True)
+    sigma=sp.linalg.diagsvd(s,len(A[:,0]),len(A[0,:])) # diagonal matrix with singular values
+    
+    return (U,sigma,V)
+    
     
 
-    # Make new matrix sigma for red, green and blue respectively 
-    Sig_red_new=Sr.copy()
-    Sig_green_new=Sg.copy()
-    Sig_blue_new=Sb.copy()
-    
-    # Then make the first n element as nonzero
-    Sig_red_new[n:800]=np.zeros_like(Sr[n:800])
-    Sig_green_new[n:800]=np.zeros_like(Sg[n:800])
-    Sig_blue_new[n:800]=np.zeros_like(Sb[n:800])
-
-    # dimension of sigma from (800,1) to (800,1000)to (800,1000)
-    Sig_red_new = sp.linalg.diagsvd(Sig_red_new,800,1000)
-    Sig_green_new = sp.linalg.diagsvd(Sig_green_new,800,1000)
-    Sig_blue_new = sp.linalg.diagsvd(Sig_blue_new,800,1000)
-
-    # now we can perform dot multiplication to create lower resolution matrix 
-    new_red = np.dot(np.dot(Ur,Sig_red_new), Vr)
-    new_green = np.dot(np.dot(Ug, Sig_green_new), Vg)
-    new_blue = np.dot(np.dot(Ub, Sig_blue_new), Vb) 
-    
-    # to construct new resolution matrix
-    img[:,:,0]= new_red
-    img[:,:,1]= new_green
-    img[:,:,2]= new_blue
-
-    #plot the image 
-    fig2 = plt.figure(n)
-    ax1 = fig2.add_subplot(2,2,1)
-    ax2 = fig2.add_subplot(2,2,2)
-    ax3 = fig2.add_subplot(2,2,3)
-    ax4 = fig2.add_subplot(2,2,4)
-    ax1.imshow(img)
-    ax2.imshow(r, cmap = 'Reds')
-    ax3.imshow(g, cmap = 'Greens')
-    ax4.imshow(b, cmap = 'Blues')
-    plt.show()
+img=mpimg.imread('Image.jpg')# load img1.jpg
+[r,g,b] = [img[:,:,i] for i in range(3)] #split the coloured image to its rgb channels
 
 
-#the original image
-#read image
-img=mpimg.imread('microsoft.jpg')
-# generate rgb array
-[r,g,b] = [img[:,:,i] for i in range(3)]
-print("Original image")
 fig = plt.figure(1)
 ax1 = fig.add_subplot(2,2,1)
 ax2 = fig.add_subplot(2,2,2)
@@ -63,21 +41,52 @@ ax4.imshow(b, cmap = 'Blues')
 plt.show()
 
 
-Ur, Sr, Vr = sp.linalg.svd(r) #to find U, sigma and V for red matrix
-Ug, Sg, Vg = sp.linalg.svd(g) #to find U, sigma and V for green matrix
-Ub, Sb, Vb = sp.linalg.svd(b) #to find U, sigma and V for blue matrix
+# original image decomposition
 
-#to find the none zero elements in sigma of each red, green and blue matrices
-red_nonzero=np.count_nonzero(Sr)
-green_nonzero=np.count_nonzero(Sg)
-blue_nonzero=np.count_nonzero(Sb)
-print("The number of non zero elements in original Sigma of red, green, blue matrices are",red_nonzero,"," ,blue_nonzero,"and" ,green_nonzero, "respectively.")
+U_red, sigma_red, V_red =decomposition(r) #decomposition for red matix
+U_green, sigma_green, V_green =decomposition(g) #decomposition for green matrix
+U_blue, sigma_blue, V_blue =decomposition(b) # decomposition for blue matrix
 
 
-#lower resolution with n=30
-print("When n = 30， lower resolution")
-svd(30)
+### compress - lower resolution picture  n=30
 
-#better resolution with n=200
-print("When n = 200, better resolution")
-svd(200)
+red_low=reso(30,U_red,sigma_red,V_red)  #compress red matrix
+green_low=reso(30,U_green,sigma_green,V_green) # compress green matrix
+blue_low=reso(30,U_blue,sigma_blue,V_blue) #compress blue matrix
+
+rgb_low = np.dstack((red_low,green_low,blue_low)) #combining the rgb channels
+fig = plt.figure(2)
+ax1 = fig.add_subplot(2,2,1)
+ax2 = fig.add_subplot(2,2,2)
+ax3 = fig.add_subplot(2,2,3)
+ax4 = fig.add_subplot(2,2,4)
+ax1.imshow(rgb_low)
+ax2.imshow(red_low, cmap = 'Reds')
+ax3.imshow(green_low, cmap = 'Greens')
+ax4.imshow(blue_low, cmap = 'Blues')
+plt.show()
+fig.savefig('low_plots.jpg')
+low_im=Image.fromarray(rgb_low,'RGB')
+low_im.save('rgb_low.jpg') #saving the lower resolution image as rgb_low.jpg
+
+
+### compress - better resolution picture n=200
+
+red_better=reso(200,U_red,sigma_red,V_red)  # compress red matrix
+green_better=reso(200,U_green,sigma_green,V_green)  #compress green matrix
+blue_better=reso(200,U_blue,sigma_blue,V_blue)  # compress blue matrix
+
+rgb_better = np.dstack((red_better,green_better,blue_better)) #combining the rgb channels
+fig = plt.figure(3)
+ax1 = fig.add_subplot(2,2,1)
+ax2 = fig.add_subplot(2,2,2)
+ax3 = fig.add_subplot(2,2,3)
+ax4 = fig.add_subplot(2,2,4)
+ax1.imshow(rgb_better)
+ax2.imshow(red_better, cmap = 'Reds')
+ax3.imshow(green_better, cmap = 'Greens')
+ax4.imshow(blue_better, cmap = 'Blues')
+plt.show()
+fig.savefig('better_plots.jpg')
+better_im=Image.fromarray(rgb_better,'RGB')
+better_im.save('rgb_better.jpg') #saving the better resolution image as rgb_better.jpg
